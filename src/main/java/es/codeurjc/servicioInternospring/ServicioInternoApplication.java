5f08b7e4fc83737eae91f67ea1bb01ba844f8c0f
sstsanta <sstsanta@gmail.com>
2022-03-23 20:39:19 +0100

    nuevos cambios de pruebas

--- src/main/java/es/codeurjc/servicioInternospring/ServicioInternoApplication.java
+++ src/main/java/es/codeurjc/servicioInternospring/ServicioInternoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServicioInternoApplication implements CommandLineRunner{
 	
 	private static final String MESSAGE = "Hello world!";
+	
 	@Autowired
     RabbitTemplate rabbitTemplate;
 
