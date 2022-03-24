54f26e446d8b31d69176bf02b578fd6134c21a1b
sstsanta <sstsanta@gmail.com>
2022-03-24 12:42:34 +0100

    nuevos cambios, para la union con el servidor de la web

--- src/main/java/es/codeurjc/servicioInternospring/Receiver.java
+++ src/main/java/es/codeurjc/servicioInternospring/Receiver.java
@@ -1,10 +1,12 @@
 package es.codeurjc.servicioInternospring;
 
+import java.util.HashMap;
+
 public class Receiver {
  
     public static final String RECEIVE_METHOD_NAME = "receiveMessage";
  
-    public void receiveMessage(String message) {
-        System.out.println("[Receiver] ha recibido el mensaje \"" + message + '"');
+    public void receiveMessage(HashMap<String,String> message) {
+        System.out.println("[Receiver] ha recibido el mensaje \"" + message.get("Nombre") + '"');
     }
 }
--- src/main/java/es/codeurjc/servicioInternospring/ServicioInternoApplication.java
+++ src/main/java/es/codeurjc/servicioInternospring/ServicioInternoApplication.java
@@ -1,5 +1,7 @@
 package es.codeurjc.servicioInternospring;
 
+import java.util.HashMap;
+
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -7,21 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServicioInternoApplication implements CommandLineRunner{
-	
-	private static final String MESSAGE = "Hello world!";
-	
-	@Autowired
-    RabbitTemplate rabbitTemplate;
+public class ServicioInternoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServicioInternoApplication.class, args);
 	}
-	
-	 @Override
-	    public void run(String... args) throws InterruptedException {
-	        System.out.println("[Application] Enviando el mensaje \"" + MESSAGE + "\"...");
-	        rabbitTemplate.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, RabbitMqConfig.ROUTING_KEY, MESSAGE);
-	    }
-
 }
